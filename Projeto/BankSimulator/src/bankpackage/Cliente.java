c1a954c28c05b798da7010dfa2a182203bc3756b
Marco Barone Belo <barone.marco@outlook.com>
2015-09-28 20:54:29 -0300

    Criado o projeto para o BankSimulator
    Criei tmb uma Classe Sistema, que vai ser o main... e criei a Classe
    Cliente

---
+++ Projeto/BankSimulator/src/bankpackage/Cliente.java
@@ -0,0 +1,15 @@
+package bankpackage;
+
+public class Cliente {
+
+		private int saldo;	
+
+		public void setSaldo(int saldo){
+			this.saldo = saldo;		
+		}
+		public int getSaldo(){
+			return this.saldo;
+		}
+
+
+}
---
+++ Projeto/BankSimulator/src/bankpackage/Sistema.java
@@ -0,0 +1,17 @@
+package bankpackage;
+
+public class Sistema {
+
+	public Sistema() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("teste");
+		Cliente cli = new Cliente();
+		cli.setSaldo(100);
+	}
+
+}
