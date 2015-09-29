564c78fd768762811fc7b3f22033b53cfa7a9dfa
Marco Barone Belo <barone.marco@outlook.com>
2015-09-28 21:18:09 -0300

    Criado o projeto em Eclipse
    Sistema é a classe main

--- Projeto/BankSimulator/src/bankpackage/Cliente.java
+++ Projeto/BankSimulator/src/bankpackage/Cliente.java
@@ -2,14 +2,20 @@ package bankpackage;
 
 public class Cliente {
 
-		private int saldo;	
-
+		private int saldo;
+		private int credito;
+		
 		public void setSaldo(int saldo){
 			this.saldo = saldo;		
 		}
 		public int getSaldo(){
 			return this.saldo;
 		}
-
+		public void setCredito(int credito){
+			this.credito = credito;
+		}
+		public int getCredito(){
+			return this.credito;
+		}
 
 }
